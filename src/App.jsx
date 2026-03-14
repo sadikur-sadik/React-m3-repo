1d0810bac8053f3eee68a597015c2fabd2da2e20
Sadikur Rahman <sadikursadikorg7@gmail.com>
2026-03-14 22:05:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Countries from './Components/country/Countries'
+
+function App() {
+
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/country/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            In the Country
+        </div>
+    );
+};
+
+export default Countries;
